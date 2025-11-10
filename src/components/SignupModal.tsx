ea296d74f162fe1ee0a314a610cdd5a0ad7cc4ef
Haris <haris.nasir0101@gmail.com>
2025-11-10 20:42:28 +0000

    fix api url

--- src/components/SignupModal.tsx
+++ src/components/SignupModal.tsx
@@ -131,7 +131,7 @@ const getMaxLength = (code) => {
         have_stock: hasInventory ? 1 : 0,
         inventory_value: hasInventory ? inventoryBand : '',
       }
-      const res = await fetch(`http://localhost:4000/api/users/Register`, {
+      const res = await fetch(`${base}/Register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', accept: 'application/json' },
         body: JSON.stringify(payload),
