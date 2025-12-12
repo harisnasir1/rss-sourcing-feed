81eef16a14fe1897fc0a641be8b011cb7a73f345
Haris <haris.nasir0101@gmail.com>
2025-12-12 17:52:53 +0000

    testing for v7

--- src/services/whatsapp_services/whatsapp.service.ts
+++ src/services/whatsapp_services/whatsapp.service.ts
@@ -130,6 +130,7 @@ export class WhatsAppClient {
 
   private async handleMessagesUpsert({ messages }: BaileysEventMap['messages.upsert']): Promise<void> {
     const msg = messages[0];
+    console.log(msg)
     if (!msg.key.fromMe && this.msg_p) {
       this.messageQueue.push(msg)
       this.processQueue()
