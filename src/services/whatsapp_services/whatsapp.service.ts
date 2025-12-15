eae9044d22020190df5d77be0ae6ef3137176c08
Haris <haris.nasir0101@gmail.com>
2025-12-15 19:22:16 +0000

    remove all msg console

--- src/services/whatsapp_services/whatsapp.service.ts
+++ src/services/whatsapp_services/whatsapp.service.ts
@@ -130,7 +130,6 @@ export class WhatsAppClient {
 
   private async handleMessagesUpsert({ messages }: BaileysEventMap['messages.upsert']): Promise<void> {
     const msg = messages[0];
-    console.log(msg)
     if (!msg.key.fromMe && this.msg_p) {
       this.messageQueue.push(msg)
       this.processQueue()
