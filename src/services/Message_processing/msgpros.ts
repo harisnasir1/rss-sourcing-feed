f2f97a4108d85b0405efbeaa778482f48e170ab7
Haris <haris.nasir0101@gmail.com>
2026-02-19 10:30:46 +0000

    Resume adding to portal

--- src/services/Message_processing/msgpros.ts
+++ src/services/Message_processing/msgpros.ts
@@ -164,7 +164,7 @@ export class Message_processing {
 
             console.log("Listing trying to be created with ->", list)
             const re = await this._rlist.create_listing(list)
-           //s const k = await this._rlist.create_listing_b2b(list, vinfo)
+            const k = await this._rlist.create_listing_b2b(list, vinfo)
             //now update the vendor
             const d = {
                 totallistings: (vinfo.totallistings || 0) + 1,
