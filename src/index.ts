88501276908f1a0749810fbca852fabf0191f6a5
Haris <haris.nasir0101@gmail.com>
2026-02-20 16:28:15 +0000

    open cors for vercel branches

--- src/index.ts
+++ src/index.ts
@@ -27,9 +27,16 @@ app.use(cors({
     
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
-    } else {
-      callback(null, false); 
     }
+    else if (/^https:\/\/sourcing-feed-git-.*\.vercel\.app$/.test(origin))
+      {
+         return callback(null, true);
+
+      }
+      else
+         {
+      callback(null, false); 
+       }
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
