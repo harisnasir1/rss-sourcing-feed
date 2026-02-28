4ec20fcc32aad9da18f27d2960701bd5ed92161f
Haris <haris.nasir0101@gmail.com>
2026-02-28 13:39:32 +0000

    cahce on brands

--- src/controllers/Filltercontroller.ts
+++ src/controllers/Filltercontroller.ts
@@ -3,17 +3,41 @@ import { GetTrueBrands } from "../utils/Brands";
 import { listing_repo } from "../repositories/listing_repo";
 
 const lr=new listing_repo()
+type brcache={
+  brands:string[],
+  expiry_time:Date,
+}
+let brand_promise:Promise<string[]>|null=null;
+let brand_cache: brcache | null = null as brcache | null;
+
 export const getAllBrands = async(req:Request , res:Response)=>{
   try {
+    const currentCache = brand_cache;
 
-     const brands=await lr.GetDistinctBrands();
-     const brandStrings = (brands || []).map((item: { brand: string }) => item.brand);
-    
-     const ubrands=GetTrueBrands(brandStrings);
-    
-     if(! ubrands) return res.status(500).json("somehitng is wrong")
+    if (currentCache && currentCache.expiry_time > new Date()) {
+      return res.status(200).json({ success: true, data: currentCache.brands });
+    }
 
-        res.status(200).json({success:true,data:ubrands})
+    brand_promise = new Promise(async(resolve,reject)=>{
+    const brands = await lr.GetDistinctBrands();
+    const brandStrings = (brands || []).map((item: { brand: string }) => item.brand);
+
+    const ubrands = GetTrueBrands(brandStrings);
+     if (!ubrands) return reject("somehting went wrong")
+       brand_cache= {
+        brands:ubrands as string[],
+        expiry_time:new Date(new Date().setHours(new Date().getHours()+2))
+    }
+    resolve( ubrands);
+    })
+    const ubrands=await brand_promise;
+
+
+
+     if (!ubrands) return res.status(500).json("somehitng is wrong")
+   
+
+    res.status(200).json({ success: true, data: ubrands })
 
   }
    catch(e)
