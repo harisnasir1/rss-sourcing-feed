173edb783d69c4cb94cefdb4f0d9ac646298ea1a
Haris <haris.nasir0101@gmail.com>
2026-03-02 13:26:03 +0000

    Vercel anaylytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { AuthProvider } from './utils/AuthContext'
-
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
